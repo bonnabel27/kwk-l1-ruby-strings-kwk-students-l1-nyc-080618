86b3751423f465a6b82c13a43833b9f9764391f1
bonnabel27 <sika.attikesse@gmail.com>
2018-08-07 14:02:27 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,7 +1,8 @@
 # Code your prompts here!
 
 puts "Hermione"
-puts 
+puts "You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30."
+puts ""
 
 
 
